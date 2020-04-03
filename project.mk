480bf0c1d4a27a8ef43956dcdd26a76055f2d16c
SadKo <lsp.plugin@gmail.com>
2020-04-03 20:30:07 +0300

    Updated version

--- project.mk
+++ project.mk
@@ -2,7 +2,7 @@
 ARTIFACT_NAME               = lsp-test-fw
 ARTIFACT_VARS               = LSP_TEST_FW
 ARTIFACT_HEADERS            = lsp-plug.in
-VERSION                     = 1.0.0
+VERSION                     = 1.0.1
 ARTIFACT_EXPORT_ALL         = 1
 
 # List of dependencies
