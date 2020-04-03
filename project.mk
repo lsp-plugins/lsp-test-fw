7cc127c25672ee6c07ec7909eecae376d78bbf12
SadKo <lsp.plugin@gmail.com>
2020-04-04 00:01:37 +0300

    Updated build

--- project.mk
+++ project.mk
@@ -5,6 +5,9 @@ ARTIFACT_HEADERS            = lsp-plug.in
 VERSION                     = 1.0.1
 ARTIFACT_EXPORT_ALL         = 1
 
+# Weak property
+DEMO_TEST                  := 1
+
 # List of dependencies
 DEPENDENCIES = \
   STDLIB \
