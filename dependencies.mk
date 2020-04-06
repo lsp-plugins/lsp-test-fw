1dfc4e7366054fcee2ba736d8cf84ca720b006d4
SadKo <lsp.plugin@gmail.com>
2020-04-06 04:27:00 +0300

    Preparing for release

--- dependencies.mk
+++ dependencies.mk
@@ -1,5 +1,5 @@
 # Variables that describe dependencies
-LSP_COMMON_LIB_VERSION     := 1.0.0
+LSP_COMMON_LIB_VERSION     := 1.0.1
 LSP_COMMON_LIB_NAME        := lsp-common-lib
 LSP_COMMON_LIB_URL         := https://github.com/sadko4u/$(LSP_COMMON_LIB_NAME).git
 
