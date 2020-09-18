92f515363b4972cb8051d9025f54de832d47e31e
sadko4u <sadko4u@gmail.com>
2020-09-18 20:40:13 +0300

    Updated source code headers to match LGPL3+

--- src/test/mtest/demo/mtest1.cpp
+++ src/test/mtest/demo/mtest1.cpp
@@ -1,8 +1,22 @@
 /*
- * mtest1.cpp
+ * Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
- *  Created on: 30 мар. 2020 г.
- *      Author: sadko
+ * This file is part of lsp-test-fw
+ * Created on: 30 мар. 2020 г.
+ *
+ * lsp-test-fw is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-test-fw is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-test-fw. If not, see <https://www.gnu.org/licenses/>.
  */
 
 
