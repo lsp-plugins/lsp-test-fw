ea31f511c663203e1faf00685d2aca838ef8766d
sadko4u <sadko4u@gmail.com>
2021-01-25 02:47:33 +0300

    Updated build

--- make/functions.mk
+++ make/functions.mk
@@ -2,20 +2,20 @@
 # Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
 #           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
 #
-# This file is part of lsp-plugin-fw
+# This file is part of lsp-test-fw
 #
-# lsp-plugin-fw is free software: you can redistribute it and/or modify
+# lsp-test-fw is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # any later version.
 #
-# lsp-plugin-fw is distributed in the hope that it will be useful,
+# lsp-runime-lib is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with lsp-plugin-fw.  If not, see <https://www.gnu.org/licenses/>.
+# along with lsp-test-fw.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 # Deduplicates all strings in the list
--- project.mk
+++ project.mk
@@ -1,7 +1,27 @@
+#
+# Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
+#           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+#
+# This file is part of lsp-test-fw
+#
+# lsp-test-fw is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# lsp-runime-lib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lsp-test-fw.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 # Package version
+ARTIFACT_ID                 = LSP_TEST_FW
 ARTIFACT_NAME               = lsp-test-fw
 ARTIFACT_DESC               = Test framework for executing automated and manual tests
-ARTIFACT_ID                 = LSP_TEST_FW
 ARTIFACT_HEADERS            = lsp-plug.in
 ARTIFACT_VERSION            = 1.0.7-devel
 ARTIFACT_EXPORT_ALL         = 1
