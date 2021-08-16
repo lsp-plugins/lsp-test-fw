7bc94e3a09c428cdb18d6c9e8b299d9ca860fe78
sadko4u <sadko4u@gmail.com>
2021-08-16 22:06:35 +0300

    Preparing for release

--- project.mk
+++ project.mk
@@ -23,7 +23,7 @@ ARTIFACT_ID                 = LSP_TEST_FW
 ARTIFACT_NAME               = lsp-test-fw
 ARTIFACT_DESC               = Test framework for executing automated and manual tests
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.8-devel
+ARTIFACT_VERSION            = 1.0.8
 ARTIFACT_EXPORT_ALL         = 1
 
 # Weak property
