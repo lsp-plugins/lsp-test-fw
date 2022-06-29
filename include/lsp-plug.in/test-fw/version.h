75aa80c1792c72fc11a0fa413b24f1a4f5111f24
sadko4u <sadko4u@gmail.com>
2022-06-29 19:30:44 +0300

    Version up

--- include/lsp-plug.in/test-fw/version.h
+++ include/lsp-plug.in/test-fw/version.h
@@ -25,6 +25,6 @@
 // Define header version
 #define LSP_TEST_FW_MAJOR           1
 #define LSP_TEST_FW_MINOR           0
-#define LSP_TEST_FW_MICRO           12
+#define LSP_TEST_FW_MICRO           13
 
 #endif /* LSP_PLUG_IN_TEST_FW_VERSION_H_ */
