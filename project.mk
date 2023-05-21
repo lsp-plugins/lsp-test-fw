3442a4270c6b81b31f537a12b627286d5c383b52
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-05-21 13:33:27 +0300

    Version up

--- project.mk
+++ project.mk
@@ -23,5 +23,5 @@ ARTIFACT_ID                 = LSP_TEST_FW
 ARTIFACT_NAME               = lsp-test-fw
 ARTIFACT_DESC               = Test framework for executing automated and manual tests
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.20
+ARTIFACT_VERSION            = 1.0.21-devel
 ARTIFACT_EXPORT_SYMBOLS     = 1
