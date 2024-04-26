d754ca1a955d0bdb3fdd9179c1d98396b7db830b
Vladimir Sadovnikov <sadko4u@gmail.com>
2024-04-27 02:53:39 +0300

    Preparing for release

--- project.mk
+++ project.mk
@@ -23,5 +23,5 @@ ARTIFACT_ID                 = LSP_TEST_FW
 ARTIFACT_NAME               = lsp-test-fw
 ARTIFACT_DESC               = Test framework for executing automated and manual tests
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.25-devel
+ARTIFACT_VERSION            = 1.0.25
 ARTIFACT_EXPORT_SYMBOLS     = 1
